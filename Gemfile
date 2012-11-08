e927b7b72f14450ccec318df998a5a4bc2719e17
yterajima <terra@e2esound.com>
2012-11-08 21:54:07 +0900

    Add Parse news data
    - 中之条町webサイトの最新情報をパース
    - ハッシュでデータを返す

--- Gemfile
+++ Gemfile
@@ -1,8 +1,3 @@
 # A sample Gemfile
 source "https://rubygems.org"
 gem "nokogiri"
-
-gem "bundler", :require => [
-  "open-uri", 
-  "logger"
-]
---
+++ nakanojyo.rb
@@ -0,0 +1,39 @@
+require "nokogiri"
+require "open-uri"
+require "date"
+
+class Nakanojyo
+  def initialize
+    @base = "http://www.town.nakanojo.gunma.jp/~info/"
+    @url  = @base + "shinchaku.html"
+    @data = Array.new
+    self.get
+  end
+
+  def get
+    begin
+      @html = Nokogiri::HTML(open(@url))
+    rescue
+      @html = ""
+    end
+  end
+
+  def parse
+    if @html != ''
+      @html.css('tr').each do |row|
+        row.css('a').each do |a|
+          date = Time.parse(row.css('font').text.strip)
+          link = a.text.strip
+          href = a['href'].strip
+          if /^http:\/\// !~ href
+            href = @base + href
+          end
+          @data.push({"date" => date, "link" => link, "href" => href})
+        end
+      end
+      @data
+    else
+      nil
+    end
+  end
+end
